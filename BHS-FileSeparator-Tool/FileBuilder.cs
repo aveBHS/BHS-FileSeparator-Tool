3de728d9bccabf252f59b51afb59db89b37e94fa
aveBHS <aveBHS.ru@gmail.com>
2020-03-07 15:15:31 +0300

    Обновление класса FileBuilder в основной программе

--- BHS-FileSeparator-Tool/FileBuilder.cs
+++ BHS-FileSeparator-Tool/FileBuilder.cs
@@ -15,6 +15,7 @@ namespace BHS_FileSeparator_Tool
         private int partsCount;
         private string md5Hash = String.Empty;
         private long partSize;
+        private string exception;
 
         public FileBuilder() { }
 
@@ -38,6 +39,7 @@ namespace BHS_FileSeparator_Tool
 
         public string FileName { get { return fileName; } set { fileName = value; } }
         public string MD5Hash { get { return md5Hash; } set { md5Hash = value; } }
+        public string Exception { get { return exception; } }
         public int PartsCount { get => partsCount; set => partsCount = value; }
         public long PartSize { get => partSize; set { partSize = value; } }
         public long FileSize { get { return size; } set { size = value; } }
@@ -61,34 +63,75 @@ namespace BHS_FileSeparator_Tool
             PartsCount++;
         }
 
-        public void Build(string path)
+        public bool Build(string path, int ramRange)
         {
-
             string[] partsMassive = new string[partsCount];
-            for (int i = 0; i < partsCount; i++)
-            {
-                partsMassive[i] = parts[i].FileName;
-            }
-            byte[] buffer = new byte[partSize];
-            FileStream outFile = new FileStream(path + FileName, FileMode.Create);
-            for (int i = 0; i < partsCount; i++)
-            {
-                /*if (!(i + 1 < partsCount))
-                {
-                    int lastPartSize = ((int)size - ((partsCount - 1) * partSize));
-                    FileStream lastSource = new FileStream(path + partsMassive[i], FileMode.Open);
-                    lastSource.Read(buffer, 0, lastPartSize);
-                    outFile.Write(buffer, 0, buffer.Length);
-                    lastSource.Close();
-                    break;
-                }*/
 
-                FileStream source = new FileStream(path + partsMassive[i], FileMode.Open);
-                source.Read(buffer, 0, (int)source.Length);
-                Console.WriteLine("Readed " + (source.Length / 1024 / 1024) + " mb");
-                outFile.Write(buffer, 0, (int)source.Length);
+            Console.WriteLine($"{Environment.NewLine}Checking part's checksums...");
+
+            for (int i = 0; i < partsCount; i++)
+            {
+                if (File.Exists(parts[i].FileName))
+                {
+                    if (FileBuilder.CalcMD5(parts[i].FileName) == parts[i].MD5Hash)
+                    {
+                        Console.WriteLine($"-- Part #{i + 1} successfully checked.");
+                        partsMassive[i] = parts[i].FileName;
+                        continue;
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(parts[i].MD5Hash))
+                        {
+                            Console.WriteLine($"-- WARGIN! Part #{i + 1} have NO checksum! It skipped for checking!");
+                            partsMassive[i] = parts[i].FileName;
+                            continue;
+                        }
+                        this.exception = $"-- Checksum checking part #{i + 1} is failed!";
+                        return false;
+                    }
+                }
+                else
+                {
+                    this.exception = $"-- Can't find part #{i + 1}. File name must be - \"{parts[i].FileName}\"!";
+                    return false;
+                }
+
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Starting building file...");
+
+            byte[] buffer = null;
+            FileStream outFile = new FileStream(FileName, FileMode.Create);
+            for (int i = 0; i < partsCount; i++)
+            {
+                FileStream source = new FileStream(partsMassive[i], FileMode.Open);
+                if (ramRange < source.Length)
+                {
+                    buffer = new byte[ramRange];
+                    for (int j = 0; j < source.Length; j += ramRange)
+                    {
+                        if (j + ramRange >= source.Length)
+                        {
+                            source.Read(buffer, 0, (int)source.Length - j);
+                            outFile.Write(buffer, 0, (int)source.Length - j);
+                            break;
+                        }
+                        source.Read(buffer, 0, ramRange);
+                        outFile.Write(buffer, 0, ramRange);
+                    }
+                }
+                else
+                {
+                    buffer = new byte[source.Length];
+                    source.Read(buffer, 0, (int)source.Length);
+                    outFile.Write(buffer, 0, (int)source.Length);
+                }
+                Console.WriteLine($"-- Writed part #{i + 1} {source.Length} bytes");
                 source.Close();
             }
+            outFile.Close();
+            return true;
         }
     }
 }
